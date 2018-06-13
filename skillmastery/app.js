67709e0a9d9eb064bbdbd9111f501f6bab2f1d82
tim <tjhickey@brandeis.edu>
2018-06-13 18:00:17 -0400

    switched to using MongoDB

--- skillmastery/app.js
+++ skillmastery/app.js
@@ -1,28 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var skillsRouter = require('./routes/skills');
+const
+ createError = require('http-errors'),
+ express = require('express'),
+ path = require('path'),
+ cookieParser = require('cookie-parser'),
+ logger = require('morgan'),
+ indexRouter = require('./routes/index'),
+ usersRouter = require('./routes/users'),
+ skillsController = require('./controllers/skillsController')
+// skillsRouter = require('./routes/skills'),
+ mongoose = require( 'mongoose' );
 
 var app = express();
+
+// here is where we connect to the database!
+mongoose.connect( 'mongodb://localhost/skillmastery' );
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//middleware to process the req object and make it more useful!
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// this handles all static routes ...
+// so don't name your routes so they conflict with the public folders
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/skills',skillsRouter);
+
+//app.use('/skills',skillsRouter);
+app.get( '/skills', skillsController.getAllSkills );
+app.post( '/saveSkill', skillsController.saveSkill );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ skillmastery/controllers/skillsController.js
@@ -0,0 +1,45 @@
+'use strict';
+const Skill = require( '../models/skill' );
+
+
+
+// this displays all of the skills
+exports.getAllSkills = ( req, res ) => {
+  Skill.find( {} )
+    .exec()
+    .then( ( skills ) => {
+      console.log("skills = "+skills)
+      res.render( 'skills', {
+        skills: skills
+      } );
+    } )
+    .catch( ( error ) => {
+      console.log( error.message );
+      return [];
+    } )
+    .then( () => {
+      console.log( 'skill promise complete' );
+    } );
+};
+
+
+
+
+exports.saveSkill = ( req, res ) => {
+  console.log("in saveSkill!")
+  console.dir(req)
+  let newSkill = new Skill( {
+    name: req.body.name,
+    description: req.body.description
+  } )
+
+  console.log("skill = "+newSkill)
+
+  newSkill.save()
+    .then( () => {
+      res.redirect( '/skills' );
+    } )
+    .catch( error => {
+      res.send( error );
+    } );
+};
