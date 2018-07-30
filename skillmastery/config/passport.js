41ff6da81ee46188bff162006d2657b1f6deeb5a
tim <tjhickey@brandeis.edu>
2018-07-30 06:53:45 -0400

    changed case of model filenames

--- skillmastery/config/passport.js
+++ skillmastery/config/passport.js
@@ -7,7 +7,7 @@
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 // load up the user model
-var User       = require('../models/user');
+var User       = require('../models/User');
 
 // load the auth variables
 var configAuth = require('./auth');
